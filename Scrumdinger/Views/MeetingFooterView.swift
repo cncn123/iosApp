109c496daa932992616682dce0d0c63ca90f63fd
Bobby <bobbycai@hk.chinamobile.com>
2023-09-25 23:07:29 +0800

    MeetingHeaderView & MeetingFooterView

--- Scrumdinger/Views/MeetingFooterView.swift
+++ Scrumdinger/Views/MeetingFooterView.swift
@@ -5,4 +5,36 @@
 //  Created by p4894 on 25/9/2023.
 //
 
-import Foundation
+import SwiftUI
+
+
+struct MeetingFooterView: View {
+    let speakers: [ScrumTimer.Speaker]
+    var skipAction: ()->Void
+    
+    private var speakerNumber: Int? {
+        guard let index = speakers.firstIndex(where: { !$0.isCompleted }) else { return nil }
+        return index + 1
+    }
+    private var isLastSpeaker: Bool {
+        return speakers.dropLast().allSatisfy { $0.isCompleted }
+    }
+    var body: some View {
+        HStack {
+            Text("Speaker 1 of 3")
+            Spacer()
+            Button(action: {}) {
+                Image(systemName: "forward.fill")
+            }
+            .accessibilityLabel("Next speaker")
+        }
+    }
+}
+
+
+struct MeetingFooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingFooterView(speakers: DailyScrum.sampleData[0].attendees.speakers, skipAction: {})
+            .previewLayout(.sizeThatFits)
+    }
+}
