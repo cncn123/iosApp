95e738c74a1285481bee7fd7ef4ffea8733cb3c7
Bobby <bobbycai@hk.chinamobile.com>
2023-09-25 18:13:03 +0800

    Extract the meeting footer

---
+++ Scrumdinger/Views/MeetingFooterView.swift
@@ -0,0 +1,8 @@
+//
+//  MeetingFooterView.swift
+//  Scrumdinger
+//
+//  Created by p4894 on 25/9/2023.
+//
+
+import Foundation
---
+++ Scrumdinger/Views/MeetingHeaderView.swift
@@ -0,0 +1,8 @@
+//
+//  MeetingHeaderView.swift
+//  Scrumdinger
+//
+//  Created by p4894 on 25/9/2023.
+//
+
+import Foundation
