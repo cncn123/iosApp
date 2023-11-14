85f13af8c721e6fcf528845181f038de4974ebd0
Bobby <bobbycai@hk.chinamobile.com>
2023-11-14 19:01:16 +0800

    add HistoryView

---
+++ Scrumdinger/Views/HistoryView.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+
+struct HistoryView: View {
+    let history: History
+    
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+
+struct HistoryView_Previews: PreviewProvider {
+    static var history: History {
+        History(attendees: [
+            DailyScrum.Attendee(name: "Jon"),
+            DailyScrum.Attendee(name: "Darla"),
+            DailyScrum.Attendee(name: "Luis")
+        ],
+                transcript: "Darla, would you like to start today? Sure, yesterday I reviewed Luis' PR and met with the design team to finalize the UI...")
+    }
+    static var previews: some View {
+        HistoryView(history: history)
+    }
+}
